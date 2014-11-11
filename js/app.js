f78aa134ed6f6378609a7e1e1f742c7f9aab18eb
GabeKagan <gabekagan@live.com>
2014-11-11 13:56:42 -0500

    Now renders something and doesn't explode
    Implemented function skeletons

--- js/app.js
+++ js/app.js
@@ -24,13 +24,28 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function () {
+    this.sprite = 'char-boy.png'; //Eventually replace this with something less... terrifying.
+}
 
+Player.prototype.update = function(dt) {
+    //Do something!
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y); //Returns TypeError
+}
+
+Player.prototype.handleInput = function() {
+    //Code this!
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+var allEnemies = []; //A glorious dawn will rise when this contains something
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
