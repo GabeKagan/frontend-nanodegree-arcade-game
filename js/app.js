e8371524b0c1cd0969add8fe9cde798b623ffcfa
GabeKagan <gabekagan@live.com>
2014-11-12 13:12:24 -0500

    Now displays a player
    You can't move yet.

--- js/app.js
+++ js/app.js
@@ -14,6 +14,9 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    //Something like this?
+    //this.x += dt;
 }
 
 // Draw the enemy on the screen, required method for game
@@ -24,8 +27,10 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function () {
-    this.sprite = 'char-boy.png'; //Eventually replace this with something less... terrifying.
+var Player = function (x,y) {
+    this.sprite = "images/char-boy.png"; //Eventually replace this with something less... terrifying.
+    this.x = x;
+    this.y = y;
 }
 
 Player.prototype.update = function(dt) {
@@ -33,7 +38,7 @@ Player.prototype.update = function(dt) {
 }
 
 Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y); //Returns TypeError
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
 Player.prototype.handleInput = function() {
@@ -44,8 +49,19 @@ Player.prototype.handleInput = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-var allEnemies = []; //A glorious dawn will rise when this contains something
-var player = new Player();
+
+var player = new Player(200,375); //Now exists.
+
+//Bloc this out.
+//var allEnemies = []; 
+//var enemy = new Enemy();
+//allEnemies.push(enemy);
+
+
+//Now we need code to spawn arbitrary enemies.
+Enemy.prototype.makeEnemy = function() {
+    //allEnemies.push(new Enemy); //I don't think this is legal Javascript code.
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -55,6 +71,7 @@ document.addEventListener('keyup', function(e) {
         38: 'up',
         39: 'right',
         40: 'down'
+
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
